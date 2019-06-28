d130e6015908d6e037b3a7923a380d14e163a4a0
Jimmy Tan <tanjimmy1129@gmail.com>
2019-06-28 16:53:09 -0400

    added events and also send email

---
+++ newsletter/mailer.rb
@@ -0,0 +1,20 @@
+require "action_mailer"
+
+# set which directory ActionMailer should use
+ActionMailer::Base.view_paths = File.dirname(__FILE__)
+
+# ActionMailer configuration
+ActionMailer::Base.smtp_settings = {
+  address:    "smtp.gmail.com",
+  port:       "587",
+  user_name:  "tanjimmy1129@gmail",
+  password:   "alomeman1",
+  authentication: :plain
+}
+
+class Newsletter < ActionMailer::Base
+  default from: "from@example.com"
+  def welcome(recipient)
+    mail(to: recipient, subject: 'Thanks for restocking')
+  end
+end
--- server.rb
+++ server.rb
@@ -36,7 +36,15 @@ end
 preme = Cake.new("Case", "Hypebeast Only!", "Price.$69.99" , "https://i.pinimg.com/originals/e4/56/36/e456361a21fe357084607ee9ef49080e.jpg")
 bf = Cake.new("Preme", "Take your case home!", "$79.99" , "https://i.pinimg.com/originals/e5/c7/73/e5c773a24777e97c9f5b2c82d6b95f00.png")
 bape = Cake.new("Milo Cake", "Take Your Monkey's Home", "$67.99" , "https://i.pinimg.com/originals/dd/11/e5/dd11e527cfba799e993f771da9027a5d.jpg")
-ones = Cookie.new("Jordan 1s" "Retro Cookie" , "$20.99" "https://dqzrr9k4bjpzk.cloudfront.net/images/12675289/811267455.jpg")
+ones = Cookie.new("Jordan 1s", "Retro Cookie", "$20.99", "https://dqzrr9k4bjpzk.cloudfront.net/images/12675289/811267455.jpg")
+zebra = Muffin.new("Yeezy-V2", "Take your shoes home", "$24.99", "https://scontent-atl3-1.cdninstagram.com/vp/dbbfc55979690fd06bfdea78f0ffa643/5D9948D7/t51.2885-15/e35/16585514_261124854298597_4817039107907649536_n.jpg?_nc_ht=scontent-atl3-1.cdninstagram.com")
+mag = Muffin.new("Air-Mag", "Back To the Future!", "$34.99", "https://pmcfootwearnews.files.wordpress.com/2017/01/nike-mag-eat-good-nyc-cupcake.jpg")
+yay = Muffin.new("Yeezy 750", "Make America Great Again", "$34.99", "http://eatgood.nyc/wp-content/uploads/2015/05/kanyewest.png")
+
+def send_email(rec)
+  Newsletter.welcome(rec).deliver_now
+end
+
 
 
 get "/" do
@@ -49,15 +57,29 @@ get "/cake" do
 end
 
 get "/cookie" do
-  @cookie = [cook, kie, ees]
   erb :cookie
 end
 
 get "/muffin" do
-  @muffin = [kanye, mag ,y3]
+  @muffin = [zebra, mag, yay]
   erb :muffin
 end
 
+def send_email(rec)
+  News.letter.welcome(rec).deliver_now
+end
+
+get "/enjoy" do
+  send_email(params[:email])
+  erb :enjoy
+end
+
+
+api_key = 'DDKTHTQAQOPATQN2FJOV'
+
 get "/events" do
+  @req = HTTParty.get("https://www.eventbriteapi.com/v3/events/search/?q=bakery&token=#{api_key}")
+  data = JSON.parse(@req.body)
+  @events = data['events']
   erb :events
 end
